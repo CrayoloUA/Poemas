66f67281a06cafd114e387fad92f429164978f32
CrayoloUA <julianvarse2006@gmail.com>
2025-04-03 23:29:41 -0500

    Update A..js

--- poemas/A..js
+++ poemas/A..js
@@ -1,1 +1,11 @@
-
+module.exports = `
+Aroma a eternidad tiene nuestro amor,
+dulce como la brisa que acaricia mis días.
+A tu lado, los problemas se desvanecen,
+y el tiempo se convierte en un susurro lejano.
+Aunque la vida nos ponga pruebas,
+nada podrá borrar lo que sentimos.
+Amar es entregarse sin miedo,
+y yo me entrego a ti, sin reservas, sin dudas.
+Amarte es mi destino, mi mayor bendición.
+`;
